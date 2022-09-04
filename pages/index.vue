c8272b8029ce4fc090765f1a6e77df6cfeb6be0c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-09-04 23:07:26 +0200

    further wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,33 @@
 <script lang="ts" setup>
-	const { data } = $(
-		await useAsyncData(async () => await trpcClient.greeting.query({ name: 'test' })),
-	);
+	import { TRPCClientError } from '@trpc/client';
 
-	if (!data) {
+	const [firstData, secondData] = await Promise.all([
+		useAsyncData('test', () => trpcClient.greeting.query({ name: 'Bla bla' })),
+		useAsyncData('test2', () => trpcClient.greeting.query({ name: 'Ble ble' })),
+	]);
+
+	const { data: greetingFirst, error: errorFirst } = $(firstData);
+	const { data: greetingSecond, error: errorSecond } = $(secondData);
+
+	if (errorFirst || errorSecond) {
 		throw createError({ statusCode: 404, statusMessage: 'Page Not Found' });
 	}
+
+	const handleClick = async () => {
+		try {
+			const data = await trpcClient.greeting.query({ name: '5' });
+
+			console.log(data);
+		} catch (error) {
+			console.log(error instanceof TRPCClientError ? error.data : '');
+		}
+	};
 </script>
 
 <template>
-	<UiHeading>{{ data }}</UiHeading>
+	<div>
+		<UiHeading>{{ greetingFirst }}</UiHeading>
+		<UiHeading>{{ greetingSecond }}</UiHeading>
+		<button @click="handleClick">Click me</button>
+	</div>
 </template>
