c6270d26f2813d7dde429ec76345de2c902f949c
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-09-06 20:41:20 +0200

    fix trpc after build

--- composables/trpcClient.ts
+++ composables/trpcClient.ts
@@ -3,7 +3,7 @@ import type { AppRouter } from '~~/server/trpc';
 import { createTRPCProxyClient } from '@trpc/client';
 
 export const trpcClient = createTRPCProxyClient<AppRouter>({
-	url: import.meta.env['SSR']
+	url: process.server
 		? `http://localhost:${process.env['NITRO_PORT'] || process.env['PORT'] || 3e3}/api/trpc`
 		: '/api/trpc',
 	headers: () => useRequestHeaders(['cookie']),
